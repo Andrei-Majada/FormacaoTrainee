1d24ad2bf1343c6093f8c8a455c39ebd863bb8b7
Andrei Schneider Majada <andreimajada@gmail.com>
2021-03-04 16:14:43 -0300

    Servidor rodando e conexão com banco de dados criada

---
+++ api-produtos-exercicio/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const models = require("./models");
+
+//Sync db
+models.sequelize
+    .sync()
+    .then(function () {
+        console.log("Database connected");
+    })
+    .catch(function (err) {
+        console.log(err, "Something went wrong with the db connection");
+    });
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set("port", port);
+const server = http.createServer(app);
+server.listen(port);
+console.log("API rodando na porta: ", port);
+
+module.exports = app;
